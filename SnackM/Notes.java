6cf38c958e4e5221fa09cf128cee6c271112be82
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-01-21 19:02:28 +0200

    commit changes classs diagram

--- SnackM/Notes.java
+++ SnackM/Notes.java
@@ -1,10 +1,14 @@
 public class Notes extends Payment {
 
     public Notes(double balance, String carrancy, String type) {
-        super(balance, carrancy, type);
+        super(balance, carrancy, type="" != null? "Notes":type );
         //TODO Auto-generated constructor stub
     }
 
+    public Notes( ) {
+        super(0.0, "Dollar", "Notes");
+        //TODO Auto-generated constructor stub
+    }
     @Override
     public void withdraw(double val) {
         // TODO Auto-generated method stub
